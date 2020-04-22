5fec83935a9fed452df379c3a6dd0af3740fbcb3
linhhm2406 <linh.hm24@gmail.com>
2020-04-22 16:28:37 +0700

    add file

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    private IOperator operator;
+
+    public Calculator(IOperator operator) {
+        this.operator = operator;
+    }
+
+    public int execute(int firstOperand, int secondOperand) {
+        return operator.execute(firstOperand, secondOperand);
+    }
+}
